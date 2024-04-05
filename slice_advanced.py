0af31f27bc44c9c021070b09bc384d7d8482b046
SimonBarilaro <162722001+SimonBarilaro@users.noreply.github.com>
2024-04-05 12:31:02 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,4 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+    objeto=input()
+    print(objeto[4::2])
+slice_advanced()
